b0a2f2c1edc425a55671385c3b4bcf5b49f77ec3
jujedev <maurimaldonado6@gmail.com>
2025-05-06 02:18:47 -0300

    example constructor

--- 74_structs/structs.go
+++ 74_structs/structs.go
@@ -2,16 +2,45 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+type user struct {
+	firstName string
+	lastName  string
+	bithDate  string
+	createdAt time.Time
+}
+
+func newUser(firstName, lastName, birthdate string) user {
+	return user{
+		firstName: firstName,
+		lastName:  lastName,
+		bithDate:  birthdate,
+		createdAt: time.Now(),
+	}
+}
+
+func (u user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.bithDate)
+}
+
+func (u *user) clearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirtDate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
+	var appUser user
+	appUser = newUser(userFirstName, userLastName, userBirtDate)
 	// ... do something awesome with that gathered data!
-
-	fmt.Println(firstName, lastName, birthdate)
+	appUser.outputUserDetails()
+	appUser.clearUserName()
+	appUser.outputUserDetails()
 }
 
 func getUserData(promptText string) string {
